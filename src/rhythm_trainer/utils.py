9f6186be0c56cddaf0df811fe1af478ff3732bfd
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2025-07-29 10:56:56 +0200

    feat: Added inferring of file format and naming scheme

--- src/rhythm_trainer/utils.py
+++ src/rhythm_trainer/utils.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
+from rhythm_trainer.config import FileFormat, NamingScheme
+
+
 def get_valid_input(prompt: str, valid_responses: list[str]) -> str:
     """Validate user input against a list of valid responses."""
     while True:
@@ -33,3 +38,56 @@ def get_number_input(
             print(f"Please enter a number less than or equal to {max_value}.")
         else:
             print("Please enter a valid number.")
+
+
+def infer_file_format(bk_tracks_dir: Path) -> FileFormat:
+    """Infer the file format from the file extension."""
+    acoustic_dir = bk_tracks_dir / "Acoustic"
+    if not acoustic_dir.is_dir():
+        raise FileNotFoundError(
+            f"Backing tracks directory {bk_tracks_dir} does not contain an 'Acoustic' "
+            f"subdirectory.",
+        )
+
+    for file in acoustic_dir.iterdir():
+        if file.is_file():
+            if file.suffix.lower() == ".mp3":
+                return FileFormat.MP3
+            if file.suffix.lower() == ".wav":
+                return FileFormat.WAV
+            raise ValueError(
+                f"Unsupported file format: {file.suffix}. "
+                f"Supported formats are {[f'.{e.value}' for e in FileFormat]}",
+            )
+
+    raise FileNotFoundError(
+        f"No backing tracks found in {bk_tracks_dir} with a recognizable file format.",
+    )
+
+
+def infer_naming_scheme(bk_tracks_dir: Path) -> NamingScheme:
+    """Infer the naming scheme from the file name.
+
+    Default:
+        - {chapter} {exercise} BK.{file_format}
+    Logical:
+        - BK {chapter} {exercise:02d}.{file_format}
+    """
+    acoustic_dir = bk_tracks_dir / "Acoustic"
+    if not acoustic_dir.is_dir():
+        raise FileNotFoundError(
+            f"Backing tracks directory {bk_tracks_dir} does not contain an 'Acoustic' "
+            f"subdirectory.",
+        )
+
+    for file in acoustic_dir.iterdir():
+        # print(f"Checking file: {file.name}")
+        if "BK" in file.name:
+            if file.name.startswith("BK "):
+                return NamingScheme.LOGICAL
+            if " BK." in file.name:
+                return NamingScheme.DEFAULT
+
+    raise FileNotFoundError(
+        f"No backing tracks found in {bk_tracks_dir} with a recognizable name.",
+    )
